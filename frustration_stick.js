f985de6b653208caf228d03e5ffb89bdefb0d33b
natsuki <nattunn.com1417@gmail.com>
2022-08-24 18:43:26 +0900

    障害物・雑完成
    全く柔軟性はないが形だけ作ったコード

--- frustration_stick.js
+++ frustration_stick.js
@@ -1,2 +1,55 @@
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
+
+/**
+ * 障害物
+ * @author なつき
+ */
+
+ ctx.beginPath();
+ ctx.rect(0, 0, 20, canvas.height);
+ ctx.fillStyle = "#0095DD";
+ ctx.fill();
+ ctx.closePath();
+
+ ctx.beginPath();
+ ctx.rect(canvas.width-20, 0, 20, canvas.height);
+ ctx.fillStyle = "#0095DD";
+ ctx.fill();
+ ctx.closePath();
+
+ ctx.beginPath();
+ ctx.rect(200, 0, 700, 20);
+ ctx.fillStyle = "#0095DD";
+ ctx.fill();
+ ctx.closePath();
+
+ ctx.beginPath();
+ ctx.rect(0, 150, 700, 20);
+ ctx.fillStyle = "#0095DD";
+ ctx.fill();
+ ctx.closePath();
+
+ ctx.beginPath();
+ ctx.rect(200, 300, 700, 20);
+ ctx.fillStyle = "#0095DD";
+ ctx.fill();
+ ctx.closePath();
+
+ ctx.beginPath();
+ ctx.rect(0, 450, 700, 20);
+ ctx.fillStyle = "#0095DD";
+ ctx.fill();
+ ctx.closePath();
+
+ ctx.beginPath();
+ ctx.rect(200, 600, 700, 20);
+ ctx.fillStyle = "#0095DD";
+ ctx.fill();
+ ctx.closePath();
+
+ ctx.beginPath();
+ ctx.rect(0, canvas.height-20, 700, 20);
+ ctx.fillStyle = "#0095DD";
+ ctx.fill();
+ ctx.closePath();
