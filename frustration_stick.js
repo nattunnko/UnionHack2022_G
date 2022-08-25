dd9ceffb387e3d0cac9f8b03308b5dc7355c99ff
sanmokusan <sanmoku.taichou@gmail.com>
2022-08-25 16:32:55 +0900

    ゴールの実装と時期のサイズ調整

--- frustration_stick.js
+++ frustration_stick.js
@@ -2,12 +2,20 @@ var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
 
 //自機の定義
-const playerHeight = 20;
-const playerWidth = 20;
+const playerHeight = 30;
+const playerWidth = 30;
 
 //自機の初期位置の定義
 let playerX = (canvas.width/2);
 let playerY = (canvas.height/2);
+
+//ゴールの定義
+const goalHeight = 100;
+const goalWidth = 100;
+
+//ゴールの位置の定義
+let goalX = (canvas.width/3); 
+let goalY =(canvas.height/3);
 
 //マウスの動作を監視
 document.addEventListener("mousemove", mouseMoveHandler, false);
@@ -28,15 +36,38 @@ function mouseMoveHandler(e){
 function drawPlayer(){
     ctx.beginPath()
     ctx.rect(playerX, playerY, playerWidth, playerHeight);
-    ctx.fillstyle = "#0095DD";
+    ctx.fillStyle = "#0095DD";
     ctx.fill();
     ctx.closePath();
 }
 
-function draw(){
-    ctx.clearRect(0, 0, 900, 750); //各フレームの前にキャンパス消去
-    drawPlayer();                  //（canvasで制御しようとしたらうまくいかなかったので取り敢えずcanvasのサイズ直接入れています）
+//ゴールを実装
+function drawGoal(){
+    ctx.beginPath();
+    ctx.rect(goalX, goalY, goalWidth, goalHeight);
+    ctx.fillStyle = "#FF0000";
+    ctx.fill();
+    ctx.closePath();
 }
 
+//ゴールしたか検知
+function goalJudge(){
+    //プレイヤーの右端がゴールの右端以上　かつ　プレイヤーの左端がゴールの右端以内であればY座標の判定に入る
+    if( (playerX + playerWidth) >= goalX && playerX <= (goalX + goalWidth) ) {
+        // プレイヤーの下端がゴールの上端以上 かつ プレイヤーの上端がゴールの下端以内であればヒットしているとみなす
+        if( (playerY + playerHeight) >= goalY && playerY <= (goalY + goalHeight) ) {
+          alert("GOAL!");
+          document.location.reload();
+          clearInterval(interval);
+        }
+      }
+    }
+
+function draw(){
+    ctx.clearRect(0, 0, 900, 750); //各フレームの前にキャンパス消去
+    drawPlayer();                  //canvasで制御しようとしたらうまくいかなかったので取り敢えず直接代入
+    drawGoal();
+    goalJudge();
+}
 //描画ループの定義
-var interval = setInterval(draw, 10);
+var interval = setInterval(draw, 5);
