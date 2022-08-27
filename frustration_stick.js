502815f27a5d945bbd78af98e33e018e10ba0dee
sanmokusan <sanmoku.taichou@gmail.com>
2022-08-27 13:50:27 +0900

    ゴール用アイテムの実装

--- frustration_stick.js
+++ frustration_stick.js
@@ -8,11 +8,12 @@ function init(){
   const keyitem_color ="#000000"
   const height = 900;
   const width = 900;
+  let keyitemNumber = 0;
 
   //自機を作成
   var player1 = new createjs.Shape(); 
   player1.graphics.beginFill(player1_color);
-  player1.graphics.drawCircle(0, 0, 30);
+  player1.graphics.drawCircle(0, 0, 20);
   stage.addChild(player1);
 
   //ゴールの作成
@@ -25,16 +26,17 @@ function init(){
   goal.graphics.lineTo(400, 400);
   stage.addChild(goal);
 
-  //空アイテムの実装
-  var keyitem = new createjs.Shape();
-  keyitem.graphics.beginFill(keyitem_color);
-  keyitem.graphics.drawPolyStar(0, 0, 40, 5, 0.6, -90);
+  
 
   //複数の星との当たり判定のために配列を作成
   var keyitemList = [];
 
   //空アイテムをランダムに配置
   for (var i = 0; i < 3; i++){
+    //空アイテムの実装
+    var keyitem = new createjs.Shape();
+    keyitem.graphics.beginFill(keyitem_color);
+    keyitem.graphics.drawPolyStar(0, 0, 40, 5, 0.6, -90);
     stage.addChild(keyitem);
     keyitem.x = 900 * Math.random();
     keyitem.y = 900 * Math.random();
@@ -59,10 +61,12 @@ function init(){
     //衝突しているか調べる
     var isHit = goal.hitTest(point.x, point.y);
 
-    //当たっていたらゴールを呼びだす
-    if(isHit == true){
+    //当たっている　かつ　アイテムを３つ取得しているならゴールを呼びだす
+    if(keyitemNumber == 3){
+      if(isHit == true){
         document.location.reload();
         clearInterval(interval);
+      }
     }
 
     // 複数のアイテムとのアタリ判定をfor文でチェックする
@@ -75,11 +79,13 @@ function init(){
       // i番目の星と自機があたっているかを調べる
       var isHit = keyitem.hitTest(point.x, point.y);
 
-      //当たっていればアイテムを消す
+      //当たっていればアイテムを消し、取得する
       if(isHit == true){
-
+        keyitem.graphics.clear();
+        keyitemNumber++;
       }
     }
+    
     //画面の更新
     stage.update();
   }
